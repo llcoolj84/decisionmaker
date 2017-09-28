64f0bb0a037061489f893daf9459c9293634d7fa
llcoolj84 <llcj84@gmail.com>
2017-09-28 19:41:36 +0000

    jquery

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,6 +1,6 @@
 $(() => {
-  $( "form" ).on( "submit", function( event ) {
-
+  $('form button')[0].on( "click", function( event ) {
+    event.preventDefault();
     let current_list = $("#myUL > li" ).serialize() ;
     console.log( current_list);
     alert(current_list);
