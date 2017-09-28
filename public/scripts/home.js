c149d4beeadc0bc2decdda22b4d3728922b248c4
llcoolj84 <llcj84@gmail.com>
2017-09-28 22:54:46 +0000

    updates to /home

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,10 +1,28 @@
 $(() => {
-  $('form button').on( "click", function( event ) {
-    event.preventDefault();
+
+  $('#create_poll').on( "submit", function( event ) {
+
+    event.preventDefault(); //Prevent default action (prevent post to /home)
+
+    // collect the body of the submit - to be done
+
+    // insert the body of the submit to db using ajax and knex
+    $.ajax({
+      method: "POST",
+      url: "/api/options"
+    }).then ({
+      // give me those two links (one result link and one link to send to other people)
+    })
+
+
     let current_list = $("#myUL > li" );
-    console.log( current_list);
     alert(current_list);
+    console.log(current_list);
+
   });
+
+
+
 });
 
 
