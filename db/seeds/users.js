d3a758ad4fe7c0e34d92fef1412b155c0846c915
Ted Yang <yangxiongtan@gmail.com>
2017-09-28 05:10:56 +0000

    finished DB read users

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -3,8 +3,8 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         knex('users').insert({first_name: 'A', last_name: 'A', email: 'aaa@aaa.com'}),
-        knex('users').insert({first_name: 'B', last_name: 'A', email: 'bbb@bbb.com'}),
-        knex('users').insert({first_name: 'C', last_name: 'A', email: 'ccc@ccc.com'})
+        knex('users').insert({first_name: 'B', last_name: 'B', email: 'bbb@bbb.com'}),
+        knex('users').insert({first_name: 'C', last_name: 'C', email: 'ccc@ccc.com'})
       ]);
     });
 };
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,10 @@ $(() => {
     url: "/api/users"
   }).done((users) => {
     for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+      $("<div>").text("User DB unique ID: " + user.id).appendTo($("body"));
+      $("<div>").text("First Name: " + user.first_name).appendTo($("body"));
+      $("<div>").text("Last Name: " + user.last_name).appendTo($("body"));
+      $("<div>").text("Email: " + user.email).appendTo($("body"));
     }
   });;
 });
