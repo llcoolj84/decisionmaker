1f3dd071fcb78a67c7c592979593c845245b4552
Ted Yang <yangxiongtan@gmail.com>
2017-10-02 05:10:18 +0000

    configured knexfile.js for production

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -4,7 +4,10 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         knex('users').insert({first_name: 'A', last_name: 'A', email: 'aaa@aaa.com'}),
         knex('users').insert({first_name: 'B', last_name: 'B', email: 'bbb@bbb.com'}),
-        knex('users').insert({first_name: 'C', last_name: 'C', email: 'ccc@ccc.com'})
+        knex('users').insert({first_name: 'C', last_name: 'C', email: 'ccc@ccc.com'}),
+        knex('users').insert({first_name: 'D', last_name: 'D', email: 'ddd@ddd.com'}),
+        knex('users').insert({first_name: 'E', last_name: 'E', email: 'eee@eee.com'}),
+        knex('users').insert({first_name: 'F', last_name: 'F', email: 'fff@fff.com'})
       ]);
     });
 };
