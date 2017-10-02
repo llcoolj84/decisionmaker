e57d8deb39f974f24618d79a2ef5d6ff98671ac1
Ted Yang <yangxiongtan@gmail.com>
2017-10-02 16:42:29 +0000

    include link in mailgun email.

--- public/scripts/mailout.js
+++ public/scripts/mailout.js
@@ -1,12 +1,17 @@
 $(() => {
 
+    function giveMeLink () {
+      var abc = window.location.href;
+      var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+      var pathArray = window.location.pathname.split('/');
+      var secondLevelLocation = pathArray[1];
+      var pollURL = "/poll/" + secondLevelLocation;
+      return pollURL;
+    }
+
     function createPollLinks(key) {
 
-        var abc = window.location.href;
-        var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
-        var pathArray = window.location.pathname.split('/');
-        var secondLevelLocation = pathArray[1];
-        var pollURL = "/poll/" + secondLevelLocation;
+        var pollURL = giveMeLink ();
         var resultURL = "/history/";
 
         var link1 = $("<a><h2>Link to Poll</h2></a>").attr('href', pollURL).attr("target", "_blank");
@@ -21,9 +26,15 @@ $(() => {
     // \\\\\\MAILGUN/////// \\
 
     // insert the body of the submit to db using ajax and knex
+    var theVoteLink = {
+      link: window.location.host + giveMeLink()
+    };
+
     $.ajax({
         method: "POST",
-        url: "/contact"
+        url: "/contact",
+        data: JSON.stringify(theVoteLink),
+        contentType: "application/json"
     }).done(function(result) { // .done for ajax HTTP request
         console.log(result);
     });
